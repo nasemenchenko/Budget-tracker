4c1d24663e0be248eb935eaff60cb301f6ec5a62
nasemenchenko <n_semenchenko@bk.ru>
2016-12-10 12:32:22 +0300

    updated model

---
+++ Logic/Entity/Budget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Entity
+{
+    class Budget
+    {
+        public int ID { get; set; }
+        public User User { get; set; }
+        public Description Description { get; set; }
+        public bool TransactionType { get; set; }
+
+    }
+    
+}
