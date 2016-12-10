9e06c0b41b5ed4f8080df2f8f463ff6d85f6fe7f
nasemenchenko <n_semenchenko@bk.ru>
2016-12-10 12:06:00 +0300

    Created entities

---
+++ Logic/Entity/Cost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Entity
+{
+    class Cost
+    {
+        public int ID { get; set; }
+        public decimal Products { get; set; }
+        public decimal Restaurants { get; set; }
+        public decimal Clothes { get; set; }
+        public decimal Fitness { get; set; }
+        public decimal CarService { get; set; }
+        public decimal Petrol { get; set; }
+        public decimal HomeItems { get; set; }
+        public decimal Rest { get; set; }
+        public decimal SmartPhone { get; set; }
+        public decimal Internet { get; set; }
+        public virtual User Users { get; set; }
+    }
+}
